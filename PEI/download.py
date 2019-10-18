13c43b74fc723738f28477d32e7cbf43975b7b2b
drizzle-zhang <zhangyu19961027@outlook.com>
2019-10-18 10:54:15 +0800

    roadmap download

--- PEI/download.py
+++ PEI/download.py
@@ -23,7 +23,7 @@ def get_links_roadmap(url):
     files_filter = []
     links = []
     for file in files:
-        if file[0] == 'E':
+        if file[-3:] == '.gz':
             files_filter.append(file)
             links.append(url + file)
 
@@ -68,11 +68,35 @@ if __name__ == '__main__':
     # narrow_files, narrow_links = get_links_roadmap(narrow_url)
     # download_data(narrow_files, narrow_links, path_out, 10)
 
-    broad_url = 'https://egg2.wustl.edu/roadmap/data/byFileType/peaks/' \
-                'consolidated/narrowPeak/ucsc_compatible/'
-    path_broad = '/home/zy/driver_mutation/data/RoadMap/broad_peak_chip_DHS'
-    broad_files, broad_links = get_links_roadmap(broad_url)
-    download_data(broad_files, broad_links, path_broad, 20)
+    # broad_url = 'https://egg2.wustl.edu/roadmap/data/byFileType/peaks/' \
+    #             'consolidated/narrowPeak/ucsc_compatible/'
+    # path_broad = '/home/zy/driver_mutation/data/RoadMap/broad_peak_chip_DHS'
+    # broad_files, broad_links = get_links_roadmap(broad_url)
+    # download_data(broad_files, broad_links, path_broad, 20)
+
+    # bigwig_url = 'https://egg2.wustl.edu/roadmap/data/byFileType/peaks/' \
+    #              'consolidated/narrowPeak/ucsc_compatible/'
+    # path_bigwig = '/home/zy/driver_mutation/data/RoadMap/bigwig_peak_chip_DHS'
+    # bigwig_files, bigwig_links = get_links_roadmap(bigwig_url)
+    # download_data(bigwig_files, bigwig_links, path_bigwig, 20)
+
+    rnaseq_url = \
+        'https://egg2.wustl.edu/roadmap/data/byDataType/rna/expression/'
+    path_rnaseq = '/home/zy/driver_mutation/data/RoadMap/Rnaseq'
+    rnaseq_files, rnaseq_links = get_links_roadmap(rnaseq_url)
+    download_data(rnaseq_files, rnaseq_links, path_rnaseq, 20)
+
+    # bigwig_url = 'https://egg2.wustl.edu/roadmap/data/byFileType/peaks/' \
+    #              'consolidated/narrowPeak/ucsc_compatible/'
+    # path_bigwig = '/home/zy/driver_mutation/data/RoadMap/bigwig_peak_chip_DHS'
+    # bigwig_files, bigwig_links = get_links_roadmap(bigwig_url)
+    # download_data(bigwig_files, bigwig_links, path_bigwig, 20)
+
+    # methy_url = 'https://egg2.wustl.edu/roadmap/data/byFileType/peaks/' \
+    #             'consolidated/narrowPeak/ucsc_compatible/'
+    # path_methy = '/home/zy/driver_mutation/data/RoadMap/methy_peak_chip_DHS'
+    # methy_files, methy_links = get_links_roadmap(methy_url)
+    # download_data(methy_files, methy_links, path_methy, 20)
 
     time_end = time.time()
     print(time_end - time_start)
