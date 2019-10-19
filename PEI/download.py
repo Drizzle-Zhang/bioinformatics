58db9fb4a9740649560499f2bff94c8bad21eaa8
drizzle-zhang <zhangyu19961027@outlook.com>
2019-10-19 21:35:10 +0800

    roadmap download

--- PEI/download.py
+++ PEI/download.py
@@ -14,7 +14,7 @@ import os
 import random
 
 
-def get_links_roadmap(url):
+def get_links_roadmap(url, suffix):
     # get download links
     web_html = urllib.request.urlopen(url)
     web_html_str = str(web_html.read())
@@ -23,7 +23,7 @@ def get_links_roadmap(url):
     files_filter = []
     links = []
     for file in files:
-        if file[-3:] == '.gz':
+        if file[-len(suffix):] == suffix:
             files_filter.append(file)
             links.append(url + file)
 
@@ -80,11 +80,11 @@ if __name__ == '__main__':
     # bigwig_files, bigwig_links = get_links_roadmap(bigwig_url)
     # download_data(bigwig_files, bigwig_links, path_bigwig, 20)
 
-    rnaseq_url = \
-        'https://egg2.wustl.edu/roadmap/data/byDataType/rna/expression/'
-    path_rnaseq = '/home/zy/driver_mutation/data/RoadMap/Rnaseq'
-    rnaseq_files, rnaseq_links = get_links_roadmap(rnaseq_url)
-    download_data(rnaseq_files, rnaseq_links, path_rnaseq, 20)
+    # rnaseq_url = \
+    #     'https://egg2.wustl.edu/roadmap/data/byDataType/rna/expression/'
+    # path_rnaseq = '/home/zy/driver_mutation/data/RoadMap/Rnaseq'
+    # rnaseq_files, rnaseq_links = get_links_roadmap(rnaseq_url)
+    # download_data(rnaseq_files, rnaseq_links, path_rnaseq, 20)
 
     # bigwig_url = 'https://egg2.wustl.edu/roadmap/data/byFileType/peaks/' \
     #              'consolidated/narrowPeak/ucsc_compatible/'
@@ -92,11 +92,25 @@ if __name__ == '__main__':
     # bigwig_files, bigwig_links = get_links_roadmap(bigwig_url)
     # download_data(bigwig_files, bigwig_links, path_bigwig, 20)
 
-    # methy_url = 'https://egg2.wustl.edu/roadmap/data/byFileType/peaks/' \
-    #             'consolidated/narrowPeak/ucsc_compatible/'
-    # path_methy = '/home/zy/driver_mutation/data/RoadMap/methy_peak_chip_DHS'
-    # methy_files, methy_links = get_links_roadmap(methy_url)
-    # download_data(methy_files, methy_links, path_methy, 20)
+    # methy_wgbs_url = 'https://egg2.wustl.edu/roadmap/data/byDataType/' \
+    #                  'dnamethylation/WGBS/FractionalMethylation_bigwig/'
+    # path_methy_wgbs = '/home/zy/driver_mutation/data/RoadMap/methy/WGBS'
+    # methy_wgbs_files, methy_wgbs_links = get_links_roadmap(methy_wgbs_url)
+    # download_data(methy_wgbs_files, methy_wgbs_links, path_methy_wgbs, 20)
+
+    # methy_rrbs_url = 'https://egg2.wustl.edu/roadmap/data/byDataType/' \
+    #                  'dnamethylation/RRBS/FractionalMethylation_bigwig/'
+    # path_methy_rrbs = '/home/zy/driver_mutation/data/RoadMap/methy/RRBS'
+    # methy_rrbs_files, methy_rrbs_links = get_links_roadmap(
+    #     methy_rrbs_url, '.bigwig')
+    # download_data(methy_rrbs_files, methy_rrbs_links, path_methy_rrbs, 20)
+
+    methy_mcrf_url = 'https://egg2.wustl.edu/roadmap/data/byDataType/' \
+                     'dnamethylation/mCRF/FractionalMethylation_bigwig/'
+    path_methy_mcrf = '/home/zy/driver_mutation/data/RoadMap/methy/mCRF'
+    methy_mcrf_files, methy_mcrf_links = get_links_roadmap(
+        methy_mcrf_url, '.bigwig')
+    download_data(methy_mcrf_files, methy_mcrf_links, path_methy_mcrf, 20)
 
     time_end = time.time()
     print(time_end - time_start)
