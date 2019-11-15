54baf4e627ae4a973ae8db2f97bf83431e8222bd
drizzle-zhang <zhangyu19961027@outlook.com>
2019-11-15 11:02:08 +0800

    annotate cRE

--- PEI/annotate_cRE/flank_cutoff.py
+++ PEI/annotate_cRE/flank_cutoff.py
@@ -23,12 +23,22 @@ def count_rows(dict_in):
     output = check_output(f"wc -l {split_out}", shell=True)
     num_rows = int(str(output).split(' ')[0][2:])
     label = path_out.split('/')[-1]
+    df_bed = pd.read_csv(split_out, sep='\t', usecols=[1, 2])
+    length = df_bed.iloc[:, 1] - df_bed.iloc[:, 0]
+    medium = length.quantile(0.5)
+    percent_75 = length.quantile(0.75)
+    percent_90 = length.quantile(0.9)
+    percent_95 = length.quantile(0.95)
+    percent_99 = length.quantile(0.99)
+    maximum = np.max(length)
 
     return {'flank_percent': flank_percent, 'num_rows': num_rows,
-            'label': label}
+            'label': label, 'medium': medium, 'percent_75': percent_75,
+            'percent_90': percent_90, 'percent_95': percent_95,
+            'percent_99': percent_99, 'maximum': maximum}
 
 
-def generate_flank_plot_file(path_in, path_flank, list_select, num_process):
+def generate_flank_plot_file(path_in, path_flank, list_select):
     if os.path.exists(path_flank):
         os.system(f"rm -rf {path_flank}")
     os.mkdir(path_flank)
@@ -55,12 +65,12 @@ def generate_flank_plot_file(path_in, path_flank, list_select, num_process):
                      accession_ids=accession_ids,
                      flank_percent=flank))
 
-    pool = Pool(processes=num_process)
+    pool = Pool(processes=80)
     func_merge = partial(merge_bed, path_in)
     pool.map(func_merge, list_input)
     pool.close()
 
-    pool = Pool(processes=num_process)
+    pool = Pool(processes=20)
     list_out = pool.map(count_rows, list_input)
     pool.close()
     df_out = pd.DataFrame(list_out)
@@ -79,20 +89,19 @@ if __name__ == '__main__':
                  ['B cell', 'adult', 'blood'],
                  ['lung', 'embryonic', 'lung'],
                  ['heart', 'child', 'heart']]
-    num_cpu = 80
     # DHS
     path_dhs = '/lustre/tianlab/zhangyu/driver_mutation/data/ENCODE/' \
                'DNase-seq/GRCh38tohg19/'
     path_dhs_flank = '/lustre/tianlab/zhangyu/driver_mutation/data/ENCODE/' \
                      'DNase-seq/GRCh38tohg19/flank'
-    generate_flank_plot_file(path_dhs, path_dhs_flank, list_test, num_cpu)
+    generate_flank_plot_file(path_dhs, path_dhs_flank, list_test)
 
     list_test = [['stomach', 'adult', 'stomach'],
                  ['layer of hippocampus', 'adult', 'brain'],
                  ['sigmoid colon', 'adult', 'intestine'],
                  ['B cell', 'adult', 'blood'],
                  ['placenta', 'embryonic', 'extraembryonic component'],
-                 ['left ventricle', 'adult', 'heart']]
+                 ['heart left ventricle', 'adult', 'heart']]
     # H3K4me3
     path_h3k4me3 = \
         '/lustre/tianlab/zhangyu/driver_mutation/data/ENCODE/' \
@@ -100,7 +109,7 @@ if __name__ == '__main__':
     path_h3k4me3_flank = \
         '/lustre/tianlab/zhangyu/driver_mutation/data/ENCODE/' \
         'histone_ChIP-seq/GRCh38tohg19/H3K4me3/flank'
-    generate_flank_plot_file(path_dhs, path_h3k4me3_flank, list_test, num_cpu)
+    generate_flank_plot_file(path_h3k4me3, path_h3k4me3_flank, list_test)
 
     # H3K27ac
     path_h3k27ac = \
@@ -109,7 +118,7 @@ if __name__ == '__main__':
     path_h3k27ac_flank = \
         '/lustre/tianlab/zhangyu/driver_mutation/data/ENCODE/' \
         'histone_ChIP-seq/GRCh38tohg19/H3K27ac/flank'
-    generate_flank_plot_file(path_dhs, path_h3k27ac_flank, list_test, num_cpu)
+    generate_flank_plot_file(path_h3k27ac, path_h3k27ac_flank, list_test)
 
     time_end = time()
     print(time_end - time_start)
